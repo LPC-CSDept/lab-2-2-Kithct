e628e938bbb148d861df0f384eaaa2fe6271b0b8
Zelin Chen <6692czl@gmail.com>
2023-01-31 17:44:08 -0800

    1st

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    WH = int(input('Working hours: '))
+    RH = 40
+    RR = 18.25
+    OR = 27.78
+    RC = RH*RR
+    OC = (WH-RH)*OR
+    TT = OC+RC
+    print("Regular Charges: ", RC)
+    print("Overtime Hours: ", OC)
+    print("Total wages: ", TT)
     pass
 
 
